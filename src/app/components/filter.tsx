fdbe89c9bb558c77a332bf3b04f988e7be18a386
Hyphenix <80988394+Hyphenix@users.noreply.github.com>
2025-03-25 12:50:11 -0700

    Promo Filter Button Functionality with useReducer

--- src/app/components/filter.tsx
+++ src/app/components/filter.tsx
@@ -1,10 +1,12 @@
 'use client'
 
 import { Button, ComboBox, Input, Label, ListBox, ListBoxItem, Popover, ToggleButton } from "react-aria-components"
-import React from "react"
-import { LocalizedStringProvider } from "react-aria-components/i18n"
+import React, { useReducer } from "react"
+import { filterReducer, initialFilters, ReducerActionType} from "../hooks/filterReducer"
 
 const FilterComponent = () => {
+    const [filters, filterDispatch] = useReducer(filterReducer, initialFilters);
+
     return (
         <>
             <div className="flex m-3 p-2 flex-wrap">
@@ -25,14 +27,24 @@ const FilterComponent = () => {
                 </div>
 
                 <div className="basis-1/2">
-                    <div>
+                    <div className="promoFilterToggles">
                         <h1 className="font-bold">Promo Types</h1>
-                        <ToggleButton className={"p-3 m-2"}>MV</ToggleButton>
+                        
+                        <ToggleButton className={"p-3 m-2"}
+                            isSelected={filters.promoType.includes("mv")}
+                            onPress={() => {
+                                filterDispatch({
+                                    type: ReducerActionType.promoFilterEvent,
+                                    payload: {category: "promoType", value: "mv", preValue: filters.promoType.includes("mv")}
+                                })
+                            }}
+                        >MV</ToggleButton>
+                        
                         <ToggleButton className={"p-3 m-2"}>Teaser</ToggleButton>
                         <ToggleButton className={"p-3 m-2"}>Visualizer</ToggleButton>
                     </div>
 
-                    <div>
+                    <div className="songFilterToggles">
                         <h1 className="font-bold">Song Types</h1>
                         <ToggleButton className={"p-3 m-2"}>Album</ToggleButton>
                         <ToggleButton className={"p-3 m-2"}>EP</ToggleButton>
--- src/app/hooks/filterReducer.tsx
+++ src/app/hooks/filterReducer.tsx
@@ -1,2 +1,48 @@
+import PreviousMap_ from "postcss/lib/previous-map";
 import React from "react";
 
+export const initialFilters = {
+    artist: "",
+    title: "",
+    releaseDate: "",
+    promoType: [] as string[],
+    songType: [] as string[]
+}
+
+export const enum ReducerActionType {
+    promoFilterEvent, songFilterEvent
+}
+
+type ReducerAction = {
+    type: ReducerActionType
+    payload: {category: "promoType" | "songType", value: string, preValue: boolean}
+}
+
+export const filterReducer = (state: typeof initialFilters, action: ReducerAction):typeof initialFilters => {
+    const {category, value, preValue} = action.payload;
+    var updatedFilters: string[] = [];
+
+    switch(action.type) {
+        case ReducerActionType.promoFilterEvent:
+            if (category == "promoType") {
+                if (preValue) {
+                    updatedFilters = state[category].filter((element) => element !== value);
+                } else {
+                    updatedFilters = [...state[category], value];
+                }
+            }
+            //console.log(state);
+            return {...state, [category]: updatedFilters};
+        case ReducerActionType.songFilterEvent:
+            if (category == "songType") {
+                if (preValue) {
+                    updatedFilters = state[category].filter((element) => element !== value);
+                } else {
+                    updatedFilters = [...state[category], value];
+                }
+            }
+            return {...state, [category]: updatedFilters};
+        default:
+            return state;
+    }
+}
