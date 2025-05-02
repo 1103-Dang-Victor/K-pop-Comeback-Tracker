1faef4255e3dcf9a476b7aba41a6c4315b36f3e2
Hyphenix <158086982+1103-Dang-Victor@users.noreply.github.com>
2025-05-02 10:35:28 -0700

    complete logic for applying filters and showing in eventCard implemented

--- src/app/components/eventCard.tsx
+++ src/app/components/eventCard.tsx
@@ -6,14 +6,16 @@ import { applyFiltersLogic } from "../scripts/filterApplyFiltersLogic";
 const EventCardComponent = () => {
     const {filterContextState} = useFilterContext();
     let matchedData = applyFiltersLogic();
-
+    console.log(matchedData.length);
     return  (
         <div className={'m-2 p-2 min-h-96'}>
             {(filterContextState.artist.length === 0 && filterContextState.promoType.length == 0
             && filterContextState.songType.length === 0) ? <p>No filters selected yet. Please select your filters.</p> 
                 : (filterContextState.filtersActive) ? (
                     <>
-                        {(matchedData.length > 0) ? (<p>{matchedData.join("||")}</p>) : (<p>No Matches Found.</p>)}
+                        {(matchedData.length > 0) ? (matchedData.map((singleResult) => {
+                            return <p key={singleResult.identifier}>{singleResult.value}</p>
+                        })) : (<p>No Matches Found.</p>)}
                     </>)
                         : <p>Filters have been selected. Please hit apply.</p>}
             
--- src/app/scripts/filterApplyFiltersLogic.tsx
+++ src/app/scripts/filterApplyFiltersLogic.tsx
@@ -1,13 +1,19 @@
 import { useFilterContext } from "../hooks/filterContext";
 import {filterJSONParser} from "./filterJSONParser";
-import { FilterState } from "../hooks/filterReducer";
+import { randomUUID } from "crypto";
 //for filtering through the selected filters and matching them to an object that can be displayed in eventCard
+//add fix later for artists selected being empty but filters are selected to 'require at least 1 artist to be selected' text on eventCard
 
+export type parsedItem = {
+  value: string,
+  identifier: string,
+  includedTypes: string;
+}
 
 export const applyFiltersLogic = () => {
   const data = filterJSONParser();
   const {filterContextState} = useFilterContext();
-  let results: string[] = [];
+  let results: parsedItem[] = [];
 
   if (filterContextState.filtersActive) {
     Object.entries(data).forEach(([groupName, releases]) => {
@@ -17,24 +23,16 @@ export const applyFiltersLogic = () => {
             const songMatch = (release.songType.length === 0) || (filterContextState.songType.includes(release.songType.toLowerCase()));
             
             if (promoMatch && songMatch) {
-              results.push(`${release.title} by ${release.artist}
-                            \n Date: ${release.releaseDate}\
-                            \n Types: ${release.promoType}, ${release.songType}`)
-            } 
+              let newParsedItem:parsedItem = {
+                identifier: crypto.randomUUID(),
+                value: `${release.title} by ${release.artist}`,
+                includedTypes: `${release.songType}, ${release.promoType}`
+              };
+              results.push(newParsedItem);
+              
+            }  
         })
-  
-      } else {
-        releases.forEach((release) => {
-          const promoMatch = (release.promoType.length === 0) || (filterContextState.promoType.includes(release.promoType.toLowerCase()));
-          const songMatch = (release.songType.length === 0) || (filterContextState.songType.includes(release.songType.toLowerCase()));
-          
-          if (promoMatch && songMatch) {
-            results.push(`${release.title} by ${release.artist}
-                          \n Date: ${release.releaseDate}\
-                          \n Types: ${release.promoType}, ${release.songType}`)
-          } 
-        })
-      }
+      } 
     });
   }
 
