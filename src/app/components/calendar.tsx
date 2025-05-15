547db7dcd410be73bbf14e86a7a67c80d8d9b0a9
Hyphenix <158086982+1103-Dang-Victor@users.noreply.github.com>
2025-05-15 12:07:06 -0700

    attempting valid calendar parsing implementation to show only matched filter dates

--- src/app/components/calendar.tsx
+++ src/app/components/calendar.tsx
@@ -2,11 +2,14 @@
 import React from "react";
 import { Button, Calendar, CalendarCell, CalendarGrid, CalendarGridBody, CalendarGridHeader,
     CalendarHeaderCell, Heading } from "react-aria-components";
+import { parseValidCalendarDates } from "../scripts/validCalendarDates";
 
 const CalendarComponent = () => {
+
     return (
         <>
-            <Calendar aria-label="MainCalendar" className={"overflow-x-hidden"}>
+            <Calendar aria-label="MainCalendar" className={"overflow-x-hidden"}
+                isDateUnavailable={parseValidCalendarDates}>
                 <header className="flex p-2 m-3 justify-between">
                     <Button slot="previous" className={`mx-5 p-3 border-2 border-solid rounded-lg border-tBase 
                         hover:shadow-md hover:shadow-tBase active:scale-95`}>Back</Button>
@@ -22,7 +25,8 @@ const CalendarComponent = () => {
 
                     <CalendarGridBody>
                         {(date) => <CalendarCell date={date} className={`text-center p-3 m-1 rounded-md text-tBase dark:text-tBase
-                            data-[selected]:bg-accent data-[selected]:text-primary data-[outside-month]:hidden`}/>}
+                            data-[selected]:bg-accent data-[selected]:text-primary data-[outside-month]:hidden
+                            data-[unavailable]:text-gray-500 dark:data-[unavailable]:text-gray-600`}/>}
                     </CalendarGridBody>
                 </CalendarGrid>
 
--- src/app/scripts/validCalendarDates.tsx
+++ src/app/scripts/validCalendarDates.tsx
@@ -1,6 +1,17 @@
 import { DateValue } from "react-aria-components";
-import { useFilterContext } from "../hooks/filterContext";
+import { applyFiltersLogic, parsedItem } from "./filterApplyFiltersLogic";
+import { fromDate } from "@internationalized/date";
 
-export const parseValidCalendarDates = (date: DateValue) => {
+export let parseValidCalendarDates = (date: DateValue) => {
+    let matchedData:parsedItem[] = applyFiltersLogic();
+
+    let matchedDates: DateValue[] = matchedData.map(function(item) {
+        return fromDate(new Date(item.releaseDate), 'America/Los_Angeles');
+    })
+
+    let insideMatchedDates = matchedDates.some((validDate) => {
+        validDate.compare(date) === 0;
+    }) 
     
+    return !insideMatchedDates;
 }
